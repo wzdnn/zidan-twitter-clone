2f0566353c1398fdfc287cabdb1252617e411ba0
Zidan Zulkhairyan Mandalim <zidanzulkhairyan@gmail.com>
2023-04-08 03:28:19 +0700

    ganti judul

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Header from "@/components/Header";
 import Form from "@/components/Form";
 import PostFeed from "@/components/posts/PostFeed";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Zwitter</title>
+        <link rel="icon" href="/doggeicon.png" />
+      </Head>
       <Header label="Home" />
       <Form placeholder="What's happening?" />
       <PostFeed />
