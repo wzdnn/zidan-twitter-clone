64079d6c21393e1901f9514f5155d6af2bb566ed
Zidan Zulkhairyan Mandalim <zidanzulkhairyan@gmail.com>
2023-04-08 03:10:28 +0700

    Ganti lagi 1 kata jadi zwitter

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -60,7 +60,7 @@ const LoginModal = () => {
   const footerContent = (
     <div className="text-neutral-400 text-center mt-4">
       <p>
-        First time creating Twitter?
+        First time creating Zwitter?
         <span
           onClick={onToggle}
           className="
